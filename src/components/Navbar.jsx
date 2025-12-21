4d4dbcc31690b57ba1cd1d4d3d2f4d6367e75e3e
Bijoy-karmokar <bijoykarmokar320@gmail.com>
2025-12-21 16:04:39 +0600

    Navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router";
 
 const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="flex-1">
+        <NavLink to='/' className="btn btn-ghost text-2xl">Byte<span className="text-primary">Blaze</span></NavLink>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <NavLink className={({isActive})=>isActive? "text-primary font-bold" : "font-bold"} to={"/"}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink className={({isActive})=>isActive? "text-primary font-bold" : "font-bold"} to={"/blogs"}>Blogs</NavLink>
+          </li>
+          <li>
+            <NavLink className={({isActive})=>isActive? "text-primary font-bold" : "font-bold"} to={"/bookmarks"}>Bookmarks</NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
